8b618c0d91acfc6effb9b29b64984988e0fe1a22
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2025-01-21 20:06:20 +0900

    ボタンイベントにhandleClickを導入

--- src/components/atoms/priceLink/index.tsx
+++ src/components/atoms/priceLink/index.tsx
@@ -1,6 +1,6 @@
 import cc from "classcat";
-import { Link as Scroll } from "react-scroll";
-import { FC, ReactNode } from "react";
+import { scroller } from "react-scroll";
+import type { FC, ReactNode } from "react";
 
 type Props = {
   children: ReactNode;
@@ -8,20 +8,27 @@ type Props = {
 };
 
 export const PriceLink: FC<Props> = (props) => {
+  const handleClick = () => {
+    scroller.scrollTo("fee", {
+      duration: 300,
+      smooth: true,
+    });
+  };
+
   return (
-    <Scroll to="fee" smooth duration={300}>
-      <span
-        className={cc([
-          "text-[10px] text-white rounded p-1 whitespace-nowrap",
-          {
-            "bg-gray-100 text-gray-800": props.type === "free",
-            "bg-gray-500 font-semibold": props.type === "course",
-            "bg-gray-200 text-gray-800": props.type === "charge",
-          },
-        ])}
-      >
-        {props.children}
-      </span>
-    </Scroll>
+    <button
+      type="button"
+      onClick={handleClick}
+      className={cc([
+        "text-[10px] text-white rounded p-1 whitespace-nowrap",
+        {
+          "bg-gray-100 text-gray-800": props.type === "free",
+          "bg-gray-500 font-semibold": props.type === "course",
+          "bg-gray-200 text-gray-800": props.type === "charge",
+        },
+      ])}
+    >
+      {props.children}
+    </button>
   );
 };
