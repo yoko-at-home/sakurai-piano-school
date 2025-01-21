163f963e85b0dda128c95231619abd0de491047e
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2025-01-21 19:29:05 +0900

    fix error

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -1,18 +1,21 @@
-import { FC, HTMLAttributeAnchorTarget, ReactNode } from "react";
-import { Link as Scroll } from "react-scroll";
+import type { FC, ReactNode } from "react";
 import * as React from "react";
 
 type Props = {
   children: ReactNode;
-  href: HTMLAttributeAnchorTarget;
+  href: string;
 };
 
 export const ButtonNav: FC<Props> = (props) => {
   return (
-    <div className="inline-block bg-primary text-white text=lg px-10 py-6 rounded font-bold hover:pointer-events-auto">
-      <Scroll to={props.href} smooth duration={300}>
-        {props.children}
-      </Scroll>
-    </div>
+    <a
+      href={props.href}
+      // to={props.href}
+      // smooth
+      // duration={300}
+      className="inline-block bg-primary text-white text=lg px-10 py-6 rounded font-bold hover:pointer-events-auto"
+    >
+      {props.children}
+    </a>
   );
 };
